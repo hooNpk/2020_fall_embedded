d11fdf848be18262855f3834327cfcc51b70c39e
hooNpk <johnhpark11@gmail.com>
2020-10-01 15:11:36 +0900

    add switch input

--- switch_input.c
+++ switch_input.c
@@ -2,24 +2,31 @@
 #include <stdlib.h>
 #include <wiringPi.h>
 #include <unistd.h>
-#define BUTTON 2 //27
-#define LED 0 //17
+#define BUTTON 0 // 17
+#define BUTTON2 2 // 27
+
 int main(void)
 {
     if(wiringPiSetup()==-1)
 	   return -1;
 	pinMode(BUTTON,INPUT);
-	pinMode(LED, OUTPUT);
 		while(1)
 	{
-		if(digitalRead(BUTTON)==HIGH){
-			printf("Button was pressed !! \n");
-			digitalWrite(LED,HIGH);
-		} else {
-			printf("Button was NOT pressed !! \n");
-			digitalWrite(LED,LOW);
+		if (digitalRead(BUTTON) != HIGH && digitalRead(BUTTON2) != HIGH) {
+			printf("Button not pressed\n");	
 		}
-		sleep(1);
+
+		else {
+			if(digitalRead(BUTTON) == HIGH) {
+				printf("Button C pressed\n");
+			}
+
+			if(digitalRead(BUTTON2) == HIGH) {
+				printf("Button D pressed\n");
+			}
+		}
+		
+		sleep(0.08);
 	}
 	return 0;
 }
