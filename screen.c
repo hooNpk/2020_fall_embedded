a5881dd16a15787bdb1d46cdf1ff069edd724227
Doubb <doubele112@naver.com>
2020-11-05 16:27:42 +0900

    add init func

--- screen.c
+++ screen.c
@@ -11,7 +11,7 @@
 #define S_HEIGHT 64
 #define S_PAGES (S_HEIGHT/8)
 
-void ssd1306_Init(int i2c_fd);
+/** void ssd1306_Init(int i2c_fd); */
 // void ssd1306_command(int i2c_fd, uint8_tcmd);
 // void ssd1306_data(int i2c_fd, const uint8_t *data, size_tsize);
 
@@ -23,6 +23,37 @@ void ssd1306_command(int i2c_fd, uint8_t cmd) {
     if(write(i2c_fd,buffer,2)!=2) {
         printf("i2c write failed!\n");
     }
+}
+
+void ssd1306_Init(int i2c_fd) {
+		ssd1306_command(i2c_fd, 0xA8);
+		ssd1306_command(i2c_fd, 0x3f);
+
+		ssd1306_command(i2c_fd, 0xD3);
+		ssd1306_command(i2c_fd, 0x00);
+
+		ssd1306_command(i2c_fd, 0x40);
+		ssd1306_command(i2c_fd, 0xA0);
+
+		ssd1306_command(i2c_fd, 0xC0);
+
+		ssd1306_command(i2c_fd, 0xDA);
+		ssd1306_command(i2c_fd, 0x12);
+
+		ssd1306_command(i2c_fd, 0x81);
+		ssd1306_command(i2c_fd, 0x7F);
+
+		ssd1306_command(i2c_fd, 0xA4);
+
+		ssd1306_command(i2c_fd, 0xA6);
+
+		ssd1306_command(i2c_fd, 0xD5);
+		ssd1306_command(i2c_fd, 0x80);
+
+		ssd1306_command(i2c_fd, 0x8D);
+		ssd1306_command(i2c_fd, 0x14);
+
+		ssd1306_command(i2c_fd, 0xAF);
 }
 
 void ssd1306_data(int i2c_fd, const uint8_t *data, size_t size) {
