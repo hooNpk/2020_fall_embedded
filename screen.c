8aa4c9e74d8d66b8a75e6269c87e1fb9f7628024
hooNpk <johnhpark11@gmail.com>
2020-11-05 19:04:43 +0900

    good

--- screen.c
+++ screen.c
@@ -10,6 +10,7 @@
 #define S_WIDTH 128
 #define S_HEIGHT 64
 #define S_PAGES (S_HEIGHT/8)
+#define SSD1306_I2C_DEV 0x3C
 
 /** void ssd1306_Init(int i2c_fd); */
 // void ssd1306_command(int i2c_fd, uint8_tcmd);
@@ -82,12 +83,38 @@ void update_full(int i2c_fd, uint8_t *data) {
     ssd1306_data(i2c_fd,data,S_WIDTH*S_PAGES);
 }
 
+void update_area(int i2c_fd, const uint8_t* data, int x, int y, int x_len, int y_len) {
+	ssd1306_command(i2c_fd, 0x20);
+	ssd1306_command(i2c_fd, 0x0);
+
+	ssd1306_command(i2c_fd, 0x21);
+	ssd1306_command(i2c_fd, x);
+	ssd1306_command(i2c_fd, x+x_len-1);
+
+	ssd1306_command(i2c_fd, 0x22);
+	ssd1306_command(i2c_fd, y);
+	ssd1306_command(i2c_fd, y+y_len-1);
+
+	ssd1306_data(i2c_fd, data, x_len * y_len);
+}
+
 int main() {
     int i2c_fd = open("/dev/i2c-1", O_RDWR);
+
+    if(ioctl(i2c_fd, I2C_SLAVE, SSD1306_I2C_DEV) < 0) {
+	    printf("err setting i2c slave address\n");
+	    return -1;
+    }
 
     ssd1306_Init(i2c_fd);
 
     uint8_t *data = (uint8_t*)malloc(S_WIDTH*S_PAGES);
+    uint8_t *data2 = (uint8_t*)malloc(40*3);
+
+    memset(data2, 0xaa, 40*3);
+    update_area(i2c_fd, data, 64, 2, 40 ,3);
+
+    free(data2);
 
     for(int x =0;x <S_WIDTH;x++) {
         for(int y =0;y <S_PAGES;y++) {
